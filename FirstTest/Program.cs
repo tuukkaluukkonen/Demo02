e83a8ab33935beb527a4f6710b86bd28e87a2300
K2970 <K2970@student.jamk.fi>
2017-01-19 13:14:14 +0200

    Salute corrected

--- FirstTest/Program.cs
+++ FirstTest/Program.cs
@@ -10,7 +10,7 @@ namespace FirstTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Github!");
+            Console.WriteLine("Hello Githubbi!");
         }
     }
 }
